fd036af87bcc890240a5c624addf9a1964a14d86
MinKumar Kshatri <rohankshatri05@gmail.com>
2025-07-28 12:15:19 +0200

    feat: add Prettier for code formatting and update styles in globals.css and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
-'use client';
-import React from 'react';
-import './globals.css';
+"use client";
+import React from "react";
+import "./globals.css";
 
 export default function Home() {
   return (
     <>
-      <header className="navbar">
-        <button className="logo-btn">
+      <main style={{ height: "2000px", backgroundColor: "#ffffffff" }}>
 
-          
-          <img src="/images/LogoTyp1.png" alt="Logo" className="logo-img" />
 
-        </button>
-        <header className="rightBarButtons">
-          <button className="sign-in-btn">Sign In</button>
-          <button className="sign-up-btn">Sign up</button>
+        <header className="navbar">
+          <button className="logo-btn">
+            <img src="/images/LogoTyp1.png" alt="Logo" className="logo-img" />
+          </button>
+          <header className="rightBarButtons">
+            <button className="sign-in-btn">Sign In</button>
+            <button className="sign-up-btn">Sign up</button>
+          </header>
         </header>
-      </header>
 
-    <main style={{ height: '2000px' }}>
-      <h1>Connect professionals with...</h1>
-    </main>
+
+
+        <h1>Connect professionals with...</h1>
+      </main>
     </>
   );
 }
